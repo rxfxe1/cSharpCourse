84c1fe43ee0d60c3169991f4e673810f09213b7a
rxfxe1 <rafaelfilipe16@hotmail.com>
2023-11-03 22:15:30 +0000

    exercises 3_3 & 3_4 from part 1

---
+++ part_1/3_3/3_3.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Generating invoices for costumer \"Contoso Corp\" ...");
+Console.WriteLine("\nInvoice: 1021\t Complete!");
+Console.WriteLine("\nInvoice: 1022\t Complete!");
+Console.WriteLine("\nOutput Directory:");
+Console.WriteLine(@"c:\invoices");
--- part_1/challenge_1/challenge_1.cs
+++ part_1/challenge_1/challenge_1.cs
@@ -8,7 +8,7 @@ Console.WriteLine({russianMessage} $@": c:\Exercise\{projectName}\ru-RU\data.txt
 */
 
 
-/*                          SOLUTION                            */
+/*                          SOLUTION                            
 
 
 string projectName = "ACME";
@@ -18,3 +18,16 @@ Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 string russianLocation = $@"c:\Exercise\{projectName}\ru-Ru\data.txt";
 Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
+
+/*                          SOLUTION                            */
+
+
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine($@"View English output:
+    c:\Exercise\{projectName}\data.txt");
+
+Console.WriteLine($@"{russianMessage}:
+    c:\Exercise\{projectName}\data.txt");
