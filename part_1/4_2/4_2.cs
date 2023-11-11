d93a2b71d2dbe4af6fd6a7401b6fbd21d5cf73ac
rxfxe1 <rafaelfilipe16@hotmail.com>
2023-11-11 10:34:02 +0000

    exercise 4_2 second part

--- part_1/4_2/4_2.cs
+++ part_1/4_2/4_2.cs
@@ -1,4 +1,4 @@
-﻿int value = 1;
+﻿/*int value = 1;
 
 value = value + 1;
 Console.WriteLine("First increment: " + value);
@@ -17,3 +17,15 @@ Console.WriteLine("Second decrement: " + value);
 
 value--;
 Console.WriteLine("Third decrement: " + value);
+
+
+-------------------------------*/
+
+
+int value = 1;
+value++;
+
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
