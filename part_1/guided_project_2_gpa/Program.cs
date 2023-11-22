e50958b3063afe480d8abacdad6d2fc22b6e4375
rxfxe1 <rafaelfilipe16@hotmail.com>
2023-11-22 11:45:41 +0000

    contnuation

--- part_1/guided_project_2_gpa/Program.cs
+++ part_1/guided_project_2_gpa/Program.cs
@@ -1,19 +1,36 @@
-﻿string studentName = "Sophia Johnson";
+﻿string studentName = "Sophia Johnson\n";
 string course1Name = "English 101";
 string course2Name = "Algebra 101";
 string course3Name = "Biology 101";
 string course4Name = "Computer Science I";
 string course5Name = "Psychollgy 101";
 
-int
 
-int course1Credit = 3;
-int course2Credit = 3;
-int course3Credit = 4;
-int course4Credit = 4;
-int course5Credit = 3;
 
-Console.WriteLine("Student: " + studentName);
+int course1Grade = 4;
+int course2Grade = 3;
+int course3Grade = 3;
+int course4Grade = 3;
+int course5Grade = 4;
 
-Console.WriteLine("Course\t\tGrade\t\tCredit Hours");
-Console.WriteLine(course1Name + );
+
+int course1Hours = 3;
+int course2Hours = 3;
+int course3Hours = 4;
+int course4Hours = 4;
+int course5Hours = 3;
+
+Console.WriteLine("\nStudent: " + studentName);
+
+Console.WriteLine("Course\t\t\tGrade\t\tCredit Hours");
+Console.WriteLine(course1Name + $"\t\t{course1Grade}\t\t{course1Hours}");
+Console.WriteLine(course2Name + $"\t\t{course2Grade}\t\t{course2Hours}");
+Console.WriteLine(course3Name + $"\t\t{course3Grade}\t\t{course3Hours}");
+Console.WriteLine(course4Name + $"\t{course4Grade}\t\t{course4Hours}");
+Console.WriteLine(course5Name + $"\t\t{course5Grade}\t\t{course5Hours}");
+
+double GPA1Multi = course1Grade * course1Hours;
+double GPA2 = course2Grade * course2Hours;
+double GPA3 = course3Grade * course3Hours;
+double GPA4 = course4Grade * course4Hours;
+double GPA5 = course5Grade * course5Hours;
